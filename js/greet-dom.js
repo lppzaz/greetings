902826bae6858be6664926a27f8bf25315a4dc8f
lppzaz <cobus.pienaar@gmail.com>
2018-05-30 11:21:36 +0200

    changed website format and layout

---
+++ js/greet-dom.js
@@ -0,0 +1,12 @@
+// refences to all the settings input fields
+var callcostElem = document.querySelector('.callCostSetting')
+var smscostElem = document.querySelector('.smsCostSetting')
+var warningElem = document.querySelector('.warningLevelSetting');
+var criticalElem = document.querySelector('.criticalLevelSetting');
+// display elements
+var settingsCallTotal = document.querySelector('.callTotalSettings');
+var settingsSmsTotal = document.querySelector('.smsTotalSettings');
+var settingsTotalElem = document.querySelector('.totalSettings');
+// reference to buttons
+var settingsAddBtn = document.querySelector('.button')
+var updateSettingsBtn = document.querySelector('.updateSettings')
--- js/tests/greet.test.js
+++ js/tests/greet.test.js
@@ -1,9 +1,6 @@
-describe('Checks for name', function() {
-      it('sets name if not in map', function() {
-        var newInstance = SettingsBill()
-        newInstance.callCost(5);
-        newInstance.billItem('call');
-        newInstance.billItem('call')
-        assert.equal(newInstance.callsTotal(), 10)
-      });
-
+describe('Tests the functionality of my greetings app', function(){
+  it('Checks user name (Cobus)', function(){
+    var greeter = Greeting();
+    greeter.name('Cobus');
+    assert.deepEqual(greeter.userName(), 'Cobus');
+  })
